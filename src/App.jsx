3ddd98b610078dad96c3d84be09a4644d2bfdfca
Yusufbek-uz <yusufbekdeveloper@gmail.com>
2025-03-08 23:50:19 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Card from './assets/components/cards/Card'
 import Sidebar from './assets/components/sitebar/Bar'
-import Header from './assets/components/header/head'
+import Header from './assets/components/header/Head'
 import Footer from './assets/components/footer/Footer'
 import './App.css'
 
